a6c033dd4991129d0cc0d89b190bba4e9de17a1d
Rattikan Baimiden <117657803+rattikan-baimiden@users.noreply.github.com>
2023-03-01 12:04:24 +0700

    Update test_courses.py

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,11 +1,12 @@
 from fastapi.testclient import TestClient
 import sys
-sys.path.insert(0, '../07')
+sys.path.insert(0, '../05_2')
 from main import app
 
 client = TestClient(app)
 _id = None
 
+#'{"msg": "Hello World"}' use '' when not have json in actual_result
 def test_hello_msg():
     url = "/hello"
     expected_result = {"msg": "Hello World"}
@@ -17,47 +18,52 @@ def test_post_insert():
     url = "/"
     actual_result = client.post(
         url,
-        json = {
+        json= {
             "course_code": "SWE62-353",
-            "course_name" : "TTD",
-            "year" : "3",
-            "group" : 1,
-            "number" : 30,
+            "course_name": "TDD",
+            "year": 3,
+            "group": 1,
+            "number": 30
         }
     )
     expected_result = "SWE62-353"
-    global _id 
-    _id = actual_result.json()[0]['id']
+    global _id
+    _id = actual_result.json()['data'][0]['id'] 
     assert actual_result.status_code == 200
-    assert actual_result.json()[0]['course_code'] == expected_result
+    assert actual_result.json()['data'][0]['course_code'] == expected_result
 
+def test_get_all():
+    url = "/"
+    actual_result = client.get(url)
+    assert actual_result.status_code == 200
 
 def test_get_by_id():
     url = "/"+_id
     actual_result = client.get(url)
     expected_result = "SWE62-353"
     assert actual_result.status_code == 200
-    assert actual_result.json()['course_code'] == expected_result
-    
+    assert actual_result.json()['data'][0]['course_code'] == expected_result
 
+#test_update
 def test_put_x_update():
     url = "/"+_id
     actual_result = client.put(
         url,
         json = {
-            "course_code": "SWE62-3532",
+            "course_code": "SWE62-3533",
             "course_name" : "TTD",
-            "year" : "3",
+            "year" : 3,
             "group" : 1,
             "number" : 30,
         }
     )
-    expected_result = "SWE62-3532"
+    expected_result = "SWE62-3533"
     assert actual_result.status_code == 200
-    assert actual_result.json()[0]['course_code'] == expected_result
+    assert actual_result.json()['data'][0]['course_code'] == expected_result
 
+#test_delete
 def test_delete_by_id():
     url = "/"+_id
     actual_result = client.delete(url)
     assert actual_result.status_code == 200
-    assert actual_result.json()['status'] == "ok"
+    assert actual_result.json()['status'] == 'ok'
